5cf4f002f8a6488289fbe18ce973eab079ca9cea
raccoon-pi <wsx1341@gmail.com>
2024-07-07 03:19:40 +0000

    init

---
+++ go.mod
@@ -0,0 +1,32 @@
+module echomiddlewarePoc
+
+go 1.22.3
+
+toolchain go1.22.5
+
+require (
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/raccoon-mh/iamtokenvalidatorpoc v0.1.0
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx v1.2.29 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/raccoon-mh/iamtokenvalidatorpoc"
+)
+
+func init() {
+	err := iamtokenvalidatorpoc.GetPubkeyIamManager("https://example.com:5000/api/auth/certs") // mc-iam-manager certs endpoint is require, this endpoint is v0.2.0..
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Any("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	protectedPath := e.Group("/protected")
+	protectedPath.Use(isTokenValid)
+	protectedPath.Use(setUserRole)
+	protectedPath.Any("/", func(c echo.Context) error {
+		roles := strings.Join(c.Get("realmAccess").([]string), ", ")
+		return c.String(http.StatusOK, "Hello, World! Protected. your roles is :"+roles)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func isTokenValid(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accesstoken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+		err := iamtokenvalidatorpoc.IsTokenValid(accesstoken)
+		if err != nil {
+			return c.String(http.StatusUnauthorized, "Authorization is not valid..")
+		}
+		c.Set("accesstoken", accesstoken)
+		return next(c)
+	}
+}
+
+func setUserRole(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accesstoken := c.Get("accesstoken").(string)
+		claims, err := iamtokenvalidatorpoc.GetTokenClaimsByIamManagerClaims(accesstoken)
+		if err != nil {
+			return c.String(http.StatusUnauthorized, "Authorization is not valid..")
+		}
+		c.Set("realmAccess", claims.RealmAccess.Roles)
+		return next(c)
+	}
+}
